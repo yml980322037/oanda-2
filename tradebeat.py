9291e3b2106893316f94c27cc92eba683761ec07
Maciej <macszop@gmail.com>
2016-02-18 23:26:42 +0000

    added save to file

--- tradebeat.py
+++ tradebeat.py
@@ -4,9 +4,11 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import time
+import random
 
 trade_word = [u"Kr\u00F3tka po rynkowej", u"D\u0142uga po rynkowej"]
 url = "http://tradebeat.pl/"
+useragent = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:44.0) Gecko/20100101 Firefox/44.0"
 
 class get_idea_trade():
     def __init__(self):
@@ -18,7 +20,7 @@ class get_idea_trade():
 
     def check_page(self):
         with requests.Session() as c:
-            self.main_page = c.get(self.base_url)
+            self.main_page = c.get(self.base_url, headers={"User-Agent": useragent})
             self.do_soup(self.main_page, "article")
 
     def do_soup(self, page, option):
@@ -42,7 +44,6 @@ class get_idea_trade():
                 else:
                     print('Nothing new!')
                     self.try_num += 1
-                    print art.contents[5].find_all("a")[0].get("href")
             except AssertionError:
                 pass
 
@@ -64,26 +65,23 @@ class get_idea_trade():
                                "Action[login]":1, "T[_B]":''}
             l.post(url+"user", data=self.login_data, headers={"Referer": url+"user"})
             page = l.get(url+link[1:])
-            print page.url
             self.do_soup(page, "p")
+
+    def save_to_file(self, data):
+        self.time = time.ctime().split(' ')
+        self.file_name = "trade-ideas."+self.time[2]+"."+self.time[1]
+        with open(self.file_name, "ab") as f:
+            f.write(data+"\n")
+
 
 try:
     p1 = get_idea_trade()
     while True:
         p1.check_page()
-        time.sleep(300)
+        time.sleep(random.randint(300,400))
 except KeyboardInterrupt:
+    for e in p1.found_list.keys():
+        p1.save_to_file(str(e))
+        p1.save_to_file(str(p1.found_list[e]))
     sys.exit(1)
 
-    #url = "http://tradebeat.pl/user"
-    #user = "maciej.szopinski@gmail.com"
-    #c.get(url)
-    #coockie = c.cookies["PHPSESSID"]
-    #login_data = {"PHPSESSID": coockie,"T[_U]": user, "T[_P]": password, "T[_A]":0, "T[_A]":1, "Action[login]":1, "T[_B]":''}
-    #c.post(url, data=login_data, headers={"Referer": "http://tradebeat.pl/user"})
-    #page = c.get("http://tradebeat.pl/dax-trade-idea-16-02-zamkniEta/")
-    #soup = BeautifulSoup(page.content, "lxml")
-    #for p in soup.find_all("p"):
-    #    print p.text
-    # u"Kr\u00F3tka po rynkowej"
-    # u"D\u0142uga po rynkowej" in article[3].text
